8b2434d56b657c41389c511401d3716eb7e6df20
Abhinav Baid <abhinavbaid@gmail.com>
2022-02-06 19:50:23 +0000

    start HidDeviceService on boot

--- service.sh
+++ service.sh
@@ -3,3 +3,12 @@ mdir = $(magisk --path)
 
 cp "$MODPATH/system/etc/sysconfig/component-overrides.xml" "$mdir/.magisk/mirror/system/etc/sysconfig"
 
+until [[ $(getprop sys.boot_completed) = 1 ]] && [[ $(getprop dev.bootcomplete) = 1 ]] && [[ $(getprop service.bootanim.exit) = 1 ]] && [[ $(getprop init.svc.bootanim) = stopped ]];  do
+sleep 3
+done
+
+sleep 1
+
+until am startservice com.android.bluetooth/.hid.HidDeviceService; do
+sleep 1
+done
